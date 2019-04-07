5dc2b25e02bc6426bd2a7cb4cade6cadc0e9fe61
yuanxiaomin <yuanxiaomin1995@126.com>
2019-04-07 13:36:59 +0800

    完善

--- Recommend/src/main/java/AlsEvaluation.scala
+++ Recommend/src/main/java/AlsEvaluation.scala
@@ -5,7 +5,7 @@ import org.joda.time.{DateTime, Duration}
 
 object AlsEvaluation {
 
-  def pepareData2(): (RDD[Rating],RDD[Rating],RDD[Rating]) = {
+  def pepareData2(): (RDD[Rating],RDD[Rating],RDD[Rating],Map[Int,String]) = {
     val sc = new SparkContext(new SparkConf().setAppName("evaluation"))
     val rawUserData = sc.textFile("hdfs://master:9000/user/hduser/test/u.data")
     val rawRating = rawUserData.map(x => x.split("\t").take(3))
@@ -15,7 +15,7 @@ object AlsEvaluation {
     val movieTitle = itemRDD.map(x => x.split("\\|").take(2)).map(x => (x(0).toInt,x(1))).collectAsMap().toMap
 
     val Array(trainData,validData,testData) = ratingRDD.randomSplit(Array(0.8,0.1,0.1))
-    return (trainData,validData,testData)
+    return (trainData,validData,testData,movieTitle)
   }
 
 
@@ -23,7 +23,7 @@ object AlsEvaluation {
     val predictRDD = model.predict(ratingRDD.map(x => (x.user,x.product)))
     val predictAndActual = predictRDD.map(x => ((x.user,x.product),x.rating)).join(ratingRDD.map(x => ((x.user,x.product),x.rating))).values
     val sumNum = ratingRDD.count()
-    return math.sqrt(predictAndActual.map(x => (x(2)-x(1))*(x(2)-x(1))).reduce(_+_)/sumNum)
+    return math.sqrt(predictAndActual.map(x => (x(2)-x(1))*(x(2)-x(1))).reduce(_+_) / sumNum)
   }
 
   def trainModel(trainData: RDD[Rating], validData: RDD[Rating], rank:Int, numItera:Int, lam:Double): (Double,Double)={
@@ -40,21 +40,30 @@ object AlsEvaluation {
 
 
 
-  def evaluatePara(trainData:RDD[Rating], validData:RDD[Rating], evaParam:String,
-                   rankArr:Array[Int],numIteraArr:Array[Int],lambdaArr:Array[Double])={
-    for (rank <- rankArr; numItera <- numIteraArr; lam <- lambdaArr){
-      val (rmse, time) =trainModel(trainData,validData,rank,numItera,lam)
-      
+  def evaluatePara(trainData:RDD[Rating], validData:RDD[Rating], rankArr:Array[Int],numIteraArr:Array[Int],lambdaArr:Array[Double]): MatrixFactorizationModel={
+    val evaluations = for (rank <- rankArr; numItera <- numIteraArr; lam <- lambdaArr) yield {
+      val (rmse,times) = trainModel(trainData,validData,rank,numItera,lam)
+      (rank,numItera,lam,rmse)
     }
+    val eval = (evaluations.sortBy(_._4))
+    val bestEval = eval(0)
+    println("最佳model参数: rank is"+bestEval._1+",iterations"+bestEval._2+",lambda is"+bestEval._3)
+    val bestModel = ALS.train(trainData,bestEval._1,bestEval._2,bestEval._3)
+    return bestModel
   }
 
   def trainValid(trainData:RDD[Rating],validData:RDD[Rating]):MatrixFactorizationModel = {
-
+    println("网格搜索最优参数")
+    val bestModel = evaluatePara(trainData,validData,Array(7,9,11),Array(10,15,20),Array(0.01,0.05,0.1))
+    return bestModel
   }
 
   def main(args: Array[String]): Unit = {
     Recommend.SetLogger
-
+    val (trainData,validData,testData,movieTitle) = pepareData2()
+    val bestModel = trainValid(trainData,validData)
+    val rmseResult = computeRMSE(bestModel,testData)
+    println("最终的test的rmse的结果是"+rmseResult)
 
   }
 }
