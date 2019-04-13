98561ab7f978508d442de8bd14a67e6e2caa8273
yuanxiaomin <yuanxiaomin1995@126.com>
2019-04-13 16:59:54 +0800

    scaler data

--- RunLogisticRegression/src/main/java/RunLogisticRegression.scala
+++ RunLogisticRegression/src/main/java/RunLogisticRegression.scala
@@ -1,4 +1,6 @@
 
+import org.apache.spark.mllib.feature.StandardScaler
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
@@ -17,5 +19,19 @@ object RunLogisticRegression {
     val lines = rawData.map(x => x.split("\t"))
     val categoryMap = lines.map(fields => fields(3)).distinct().collect().zipWithIndex.toMap
 
+    val labeledPointRDD = lines.map{fields =>
+      val trimFields = fields.map(x => x.replaceAll("\"",""))
+      val categoryFeaArr = Array.ofDim[Double](categoryMap.size)
+      val categoryIndex = categoryMap(fields(3))
+      categoryFeaArr(categoryIndex) = 1
+      val numericalFea = trimFields.slice(4,fields.size-1).map{x => if (x == "?") 0.0 else x.toDouble}
+      val label = trimFields(fields.size-1).toInt
+      val features = Vectors.dense(categoryFeaArr++numericalFea)
+      LabeledPoint(label,features)
+    }
+
+    val featuresData = labeledPointRDD.map(x => x.features)
+    val stdScaler = new StandardScaler(withMean = true, withStd = true).fit(featuresData)
+    val scalerdRDD = labeledPointRDD.map(x => LabeledPoint(x.label,stdScaler.transform(x.features)))
   }
 }
